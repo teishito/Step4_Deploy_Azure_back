80bf6b1260dd210ff29206e2bfe04e3b8a7ec9d4
teishito <shito.tei@foru.co.jp>
2025-02-11 01:21:45 +0900

    Delete db_control directory

--- db_control/create_tables.py
+++
@@ -1,8 +0,0 @@
-import platform
-print(platform.uname())
-
-from mymodels import Base #, User, Comment
-from connect import engine
-
-print("Creating tables >>> ")
-Base.metadata.create_all(bind=engine)
