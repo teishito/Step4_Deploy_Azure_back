c5748658d39a5df96d7737524c95eb72d704bd7f
teishito <shito.tei@foru.co.jp>
2025-02-11 01:06:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -112,4 +112,4 @@ async def purchase(items: list[dict]):
 # アプリケーション起動
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
